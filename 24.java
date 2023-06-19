c868b57a899b3cae9b17b5f484aded934f63dd12
Anirudha Anekal <77496170+Wambyat@users.noreply.github.com>
2023-06-19 19:23:37 +0530

    Update 24.java

--- 24.java
+++ 24.java
@@ -1,8 +1,9 @@
-class Solution {
-
-    public ListNode swapPairs(ListNode head) {
-
-        if (head == null || head.next == null) {
+class Solution 
+{
+    public ListNode swapPairs(ListNode head) 
+    {
+        if (head == null || head.next == null) 
+        {
             return head;
         }
 
@@ -18,12 +19,14 @@ class Solution {
         curr = temp_ahead;
         temp = curr;
 
-        if (curr.next == null) {
+        if (curr.next == null) 
+        {
             return head;
         }
         temp_ahead = temp_ahead.next;
 
-        while (temp_ahead != null) {
+        while (temp_ahead != null) 
+        {
 
         }
 
