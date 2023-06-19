50ec51b934d8a9d39e5c9731cc48fa9569db7733
Anirudha Anekal <77496170+Wambyat@users.noreply.github.com>
2023-06-19 11:29:14 +0530

    Update 24.java

--- 24.java
+++ 24.java
@@ -1,23 +1,30 @@
 class Solution {
-    
+
     public ListNode swapPairs(ListNode head) {
-        
-        if(head == null || head.next == null)
-        {
+
+        if (head == null || head.next == null) {
             return head;
         }
 
+        ListNode curr = head;
         ListNode temp = head;
-        ListNode temp_curr = head;
-        ListNode temp_ahead = head.next;        
+        ListNode temp_ahead = head.next;
 
-        while (temp_ahead != null)
-        {
-            temp_curr.next = temp_ahead;
-            temp_ahead = temp_ahead.next;
-            temp_curr.next.next = temp;
-            temp_curr = temp_curr.next.next;
-            temp = temp_curr;
+        // First swap done outside the loop
+        curr.next = temp_ahead.next;
+        temp_ahead.next = curr;
+        head = temp_ahead;
+        temp_ahead = curr.next;
+        curr = temp_ahead;
+        temp = curr;
+
+        if (curr.next == null) {
+            return head;
+        }
+        temp_ahead = temp_ahead.next;
+
+        while (temp_ahead != null) {
+
         }
 
         return head;
